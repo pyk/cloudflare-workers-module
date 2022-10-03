267749a6a9ab1a1c8d6548381a5e9793940644ed
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2022-10-04 05:39:27 +0700

    chore: add basic router

---
+++ app/coins/[id]/page.tsx
@@ -0,0 +1,3 @@
+const Page = () => <div>This is coins page</div>;
+
+export default Page;
--- app/server.tsx
+++ app/server.tsx
@@ -4,6 +4,15 @@ import ReactDOMServer from "react-dom/server";
 // Import layout
 import RootLayout from "./layout";
 
+// Import pages
+import CoinsIdPage from "./coins/[id]/page";
+
+const CoinsIdPageMarkup = (
+    <RootLayout>
+        <CoinsIdPage />
+    </RootLayout>
+);
+
 export interface Env {}
 
 const AppManifest = {
@@ -12,15 +21,33 @@ const AppManifest = {
     },
 };
 
+const coinHandler = () => {
+    return new Response("coinHandler");
+};
+
+const patternsToHandlers = new Map([
+    [{ pathname: "/coins/:id" }, coinHandler],
+]);
+
+const routingMap = new Map();
+for (const [patternInput, handler] of patternsToHandlers) {
+    const compiledPattern = new URLPattern(patternInput);
+    routingMap.set(compiledPattern, handler);
+}
+
 export default {
     async fetch(
         request: Request,
         env: Env,
         ctx: ExecutionContext
     ): Promise<Response> {
-        const stream = await ReactDOMServer.renderToReadableStream(
-            <RootLayout />
-        );
-        return new Response(stream);
+        for (const [compiledPattern, handler] of routingMap) {
+            const result = compiledPattern.exec(request.url);
+            if (result) {
+                console.log("DEBUG: result", result);
+                return handler();
+            }
+        }
+        return new Response("Handler not found");
     },
 };
